a1497345c0282ab3acba7335b1ac1db6f45104c1
SandipChakraborty <catch2sandy007@gmail.com>
2020-08-28 13:13:12 +0530

    Initial dev branch

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -24,27 +24,4 @@ public class MyController {
         return new ResponseEntity(decoder.decode(tokenPayloads[1]), HttpStatus.OK);
     }
 
-
-    @GetMapping("/{braces}")
-    public boolean shift(@PathVariable() String braces) {
-
-//        braces.substring()
-
-        return isComplete(braces);
-    }
-
-    private boolean isComplete(String braces) {
-
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < braces.length(); i++) {
-
-            if (braces.charAt(i) == '(') {
-                stack.push('(');
-            } else {
-                if (stack.isEmpty()) return false;
-                else stack.pop();
-            }
-        }
-        return stack.isEmpty();
-    }
 }
